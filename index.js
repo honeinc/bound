f9fc7ff2b2742afac2aa9a32eb4e2f511a9912a2
Jacob Lowe <jacoblowe2.0@gmail.com>
2015-01-19 09:55:38 -0800

    Adding in a decycle of the object to remove chance of circular objects

--- index.js
+++ index.js
@@ -7,14 +7,15 @@ module.exports = _bound.bind( null, 'on', false );
 
 function noop () {}
 // here to store refs to bound functions
-var _fns = {};
+var _fns = {},
+    utils = require( './src/utils' );
 
 var bindEvent = 
 module.exports.bindEvent = function ( fn, eventName, handler, context, removeCache ) {
     if ( typeof handler !== 'function' ) {
         return;
     }
-    var _id = handler.toString() + JSON.stringify( context );
+    var _id = handler.toString() + JSON.stringify( utils.decycleObject( context ) );
     /* 
       this is to cache the function so it can be unbound from the event
       because fn.bind( ) create a new function, which mean fn === fn.bind() is false
